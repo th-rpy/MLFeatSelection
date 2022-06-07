07f504c03ef907770a9668b98ce74bd0bbb411e0
th-rpy <thamer.saraei@usherbrooke.ca>
2022-06-07 14:08:40 -0400

    Add decribe_data func

--- mlfeatselection/Datasets/get_data.py
+++ mlfeatselection/Datasets/get_data.py
@@ -1,6 +1,9 @@
+from attr import attributes
+from importlib_metadata import distribution
 import pandas as pd
 import numpy as np
 import glob, os
+from tables import Description
 from tabulate import tabulate
 import pkg_resources
 import sys
@@ -14,21 +17,23 @@ class Data:
     def __init__(self) -> None:
         self.data = None
         self.label = None
+        self.name = None
         
     def get_dataset_by_name(self, dataset_name):
         
         # function to retrieve the data
+        self.name = dataset_name
         stream = pkg_resources.resource_stream(__name__, 'OUR_DATA/' + dataset_name + '.csv')
         df = pd.read_csv(stream, header=None, sep=',')
-        self.data = np.array(df.iloc[:, 0:-1])
-        self.target = np.array(df.iloc[:, -1])
+        self.data = df.iloc[:, 0:-1]
+        self.target = df.iloc[:, -1]
         print("Requested dataset found and loaded... \n")
         print("Dataset: " + dataset_name + "\n") 
         print("Shape of data: " + str(self.data.shape) + "\n")
         print("Shape of target: " + str(self.target.shape) + "\n")
         print("==========================================\n") 
         print(df.head(5))
-        return self.data, self.target 
+        return self.data, self.target, self.name
     
     def get_dataset_names(self):
         
@@ -49,21 +54,37 @@ class Data:
     
     def get_dataset_by_path(self, dataset_path, target, delimiter=','):
         
+        self.name = dataset_path.split('/')[-1].split('.')[0]
         df = pd.read_csv(dataset_path, sep=delimiter, ) # read the dataset
         columns = df.columns # get the columns names
         assert target in columns, "Target column not found in dataset" # check if the target column is in the dataset
-        self.data = np.array(df.loc[:, df.columns != target]) # get the data
-        self.target = np.array(df[target]) # get the target
+        self.data = df.loc[:, df.columns != target] # get the data
+        self.target = df[target] # get the target
         
-        return self.data, self.target # return the data and target
+        return self.data, self.target, self.name # return the data and target
     
+    def describe_data(self): 
+        description_list = []
+        name = self.name
+        lines, columns = self.data.shape
+        attributes = self.data.columns
+        n_classes = len(np.unique(self.target))
+        distribution_classes = self.target.value_counts()
+        description_list.append([name,
+                           lines,
+                          len(attributes),
+                           n_classes,
+                           tuple(distribution_classes)])
+        print(tabulate(description_list, headers=["Dataset", "Lines", "Attributes", "Classes", "Distribution"]))
+        return description_list
+        
     def __str__(self):
         return str(self.display_datasets())
         
 d = Data()
 #d.display_datasets()
 d.get_dataset_by_path('mlfeatselection/mlfeatselection/Datasets/iris.csv', target = 'Species', delimiter=',')
-print(d.data)
+d.describe_data()
 
 """data_paths = glob.glob(os.getcwd() + "/OUR_DATA/*.csv")
 list_datasets = [os.path.splitext(os.path.basename(filename))[0] for filename in data_paths]
